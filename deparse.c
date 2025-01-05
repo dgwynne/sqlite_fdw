06e26c81193231775567b1e185082f1eade27590
David Gwynne <david@gwynne.id.au>
2025-01-06 09:53:09 +1000

    @mkgrgis points out that real affinity may need quality_col too

--- deparse.c
+++ deparse.c
@@ -2280,6 +2280,8 @@ sqlite_deparse_column_ref(StringInfo buf, int varno, int varattno, PlannerInfo *
 					else
 					{
 						elog(DEBUG2, "floatN real affinity only for \"%s\"", colname);
+						if (qualify_col)
+							ADD_REL_QUALIFIER(buf, varno);
 						appendStringInfoString(buf, sqlite_quote_identifier(colname, '`'));
 					}
 					break;
